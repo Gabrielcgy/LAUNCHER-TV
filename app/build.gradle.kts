69a1d3501b955313cd8ac5a0f5c204357dbb4235
Gabrielcgy <kb0528256@gmail.com>
2026-07-14 09:08:04 -0300

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "com.aistudio.tvlauncher.xpkfwt"
     minSdk = 23
     targetSdk = 29
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
