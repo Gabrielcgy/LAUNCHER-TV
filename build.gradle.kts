ca01a4edf4f7ed858a5271136b084e2d709a5d57
Gabrielcgy <kb0528256@gmail.com>
2026-07-13 21:34:48 -0300

    Configure Android application with SDK and dependencies
    Updated the build configuration to include Android and Kotlin plugins, set SDK versions, and defined application details.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,23 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.android) apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
-  alias(libs.plugins.roborazzi) apply false
-  alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply false
+ plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 33
+    defaultConfig {
+        applicationId = "com.example.lanzadortv"
+        minSdk = 23
+        targetSdk = 29
+        versionCode = 2
+        versionName = "2.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
